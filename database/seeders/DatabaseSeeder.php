bb2ed959fc7ccee8c49eb92a60073e46b3dc9d7f
Noval Aly <alynoval10@gmail.com>
2026-08-19 13:55:04 +0700

    Fix user roles and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,14 +13,11 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-{
-    $this->call([
-        RoleSeeder::class,
-        SettingSeeder::class,
-    ]);
-
-    $this->call([
-    AdminUserSeeder::class,
-]);
-}
+    {
+        $this->call([
+            RoleSeeder::class,
+            SettingSeeder::class,
+            AdminUserSeeder::class,
+        ]);
+    }
 }
